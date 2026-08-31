1fc495de4cf4b2e544610febbbfe3b5127d3e942
niboow <woobinwang07@gmail.com>
2026-08-31 21:07:50 +0000

    Add basic node skeleton with startup log message

--- turtle_waypoint_nav/src/waypoint_nav.cpp
+++ turtle_waypoint_nav/src/waypoint_nav.cpp
@@ -1,10 +1,23 @@
-#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+
+class WaypointNav : public rclcpp::Node
+{
+public:
+  //constructor
+  WaypointNav() : Node("waypoint_nav"){ // string gives a name to the node (initialize)
+
+    RCLCPP_INFO(this->get_logger(), "Turtle is awake!"); //parameters are logger, message string
+
+  }
+};
+
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
 
-  printf("hello world turtle_waypoint_nav package\n");
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<WaypointNav>(); //auto tells compiler to find type 
+  rclcpp::spin(node); //activates the node
+  rclcpp::shutdown();
   return 0;
 }
